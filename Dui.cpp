930da886d391817e6f0ada5fcc074cfd85978133
Pascal Barlier <contact@codeinter.net>
2018-04-03 17:27:25 +0200

    Add files via upload

--- Dui.cpp
+++ Dui.cpp
@@ -9,24 +9,24 @@
 
 byte Dui::readButtons()  // Lecture des boutons
 {
-  word keyInput = analogRead(0);  // En commentaire : les valeurs lues lors des tests
-  if (keyInput > 950) return DUI_KEY_NONE;   // 1019
-  if (keyInput < 50)   return DUI_KEY_RIGHT; // 0
-  if (keyInput < 180)  return DUI_KEY_UP;    // 98
-  if (keyInput < 330)  return DUI_KEY_DOWN;  // 256
-  if (keyInput < 530)  return DUI_KEY_LEFT;  // 411
-  if (keyInput < 760)  return DUI_KEY_ESC;   // 644
-  return DUI_KEY_OK;
+	word keyInput = analogRead(0);  // En commentaire : les valeurs lues lors des tests
+	if (keyInput > 950) return DUI_KEY_NONE;   // 1019
+	if (keyInput < 50)   return DUI_KEY_RIGHT; // 0
+	if (keyInput < 180)  return DUI_KEY_UP;    // 98
+	if (keyInput < 330)  return DUI_KEY_DOWN;  // 256
+	if (keyInput < 530)  return DUI_KEY_LEFT;  // 411
+	if (keyInput < 760)  return DUI_KEY_ESC;   // 644
+	return DUI_KEY_OK;
 }
 
-void Dui::duiSelectDisplay(byte on) // Affiche ou efface le curseur de sélection
+void Dui::selectDisplay(byte on) // Affiche ou efface le curseur de sélection
 {
-  Serial.println("duiSelectDisplay");
+  Serial.println("selectDisplay");
 /*
   if(duiActiveItem==99) return;
-  struct _duiDATA duiItem = duiCurrentPage[duiActiveItem];
+  struct _duiDATA duiItem = currentPage[duiActiveItem];
   lcd.setCursor(duiItem.x, duiItem.y);
-  switch(duiCurrentPage[duiActiveItem].type)
+  switch(currentPage[duiActiveItem].type)
   {
     case DUI_SEL : // pour le type SEL : [...]
       lcd.write(on?'[':' ');
@@ -40,127 +40,193 @@ void Dui::duiSelectDisplay(byte on) // Affiche ou efface le curseur de sélectio
 */
 }
 
-void Dui::duiChangeActive(byte action,byte index) // Change le texte actif d'un bouton
+char Dui::findObject(byte type,byte index) // Recherche un objet similaire (x,y,type)
 {
-  Serial.println("duiChangeActive");
-  struct _duiDATA *duiData = duiCurrentPage+index;
-  byte x=duiData->x;
-  byte y=duiData->y;
-  byte type=duiData->type;
-
-  char i;
-  duiData = duiCurrentPage;
-  for(i=0; duiData->type; i++) // Cherche l'objet actif
-  {
-    if((x==duiData->x)&&(y==duiData->y)&&((type|DUI_ACTIVE)==duiData->type))
-      break;
-    duiData++;
-  }
-  if(!duiData->type) return; // Pas d'objet actif
-
-  struct _duiDATA *duiFindData=duiData;
-  switch(action)
-  {
-    case DUI_PREV:
-      for(; --i>=0;) // Cherche l'objet précédent
-      {
-        duiFindData--;
-        if((x==duiFindData->x)&&(y==duiFindData->y)&&(type==duiFindData->type)&&duiFindData->text)
-        {
-          duiFindData->type|=DUI_ACTIVE;
-          duiData->type&=~DUI_ACTIVE;
-          return;
-        }
-      }
-      break;
-    case DUI_NEXT:
-      for(i++; (++duiFindData)->type; i++) // Cherche l'objet suivant
-      {
-        if((x==duiFindData->x)&&(y==duiFindData->y)&&(type==duiFindData->type)&&duiFindData->text)
-        {
-          duiFindData->type|=DUI_ACTIVE;
-          duiData->type&=~DUI_ACTIVE;
-          return;
-        }
-      }
-      break;
-  }
+	struct _duiDATA *duiData = currentPage+index;
+	byte x=duiData->x;
+	byte y=duiData->y;
+	byte type=duiData->type&~DUI_ACTIVE;
+	char i;
+	char find=-1;
+	char findActive=-1;
+	duiData = currentPage;
+	for(i=0; duiData->type; i++)
+	{
+		if((x==duiData->x)&&(y==duiData->y)&&(type==(duiData->type&~DUI_ACTIVE)))
+		{
+			switch(type)
+			{
+				case DUI_FIND_ACTIVE :
+					if(duiData->type&DUI_ACTIVE) return i;
+					break;
+				case DUI_FIND_PREV :
+					if(duiData->type&DUI_ACTIVE) return find;
+					break;
+				case DUI_FIND_NEXT :
+					if(findActive>=0) return i;
+					break;
+				case DUI_FIND_FIRST :
+					return i;
+				case DUI_FIND_LAST :
+					break;
+			}
+			find=i;
+			if(duiData->type&DUI_ACTIVE) findActive=i;
+		}
+		duiData++;
+	}
+	return find;
 }
 
-byte Dui::duiExec(byte btn) // Traite l'appui sur un bouton
+void Dui::changeActive(byte action,byte index) // Change le texte actif d'un bouton
+{
+	Serial.println("changeActive");
+	char obj=findObject(DUI_FIND_ACTIVE,index);	
+	if(obj<0) return; // Pas d'objet actif
+	char findObj=-1;
+	switch(action)
+	{
+		case DUI_PREV : findObj=findObject(DUI_FIND_PREV,obj); break;
+		case DUI_NEXT : findObj=findObject(DUI_FIND_NEXT,obj); break;
+	}
+	if(findObj>0)
+	{
+		duiData[findObj].type|=DUI_ACTIVE;
+		duiData[obj].type&=~DUI_ACTIVE;
+	}
+}
+
+/*
+void Dui::changeActive(byte action,byte index) // Change le texte actif d'un bouton
+{
+	Serial.println("changeActive");
+	struct _duiDATA *duiData = currentPage+index;
+	byte x=duiData->x;
+	byte y=duiData->y;
+	byte type=duiData->type;
+	
+	char i;
+	duiData = currentPage;
+	for(i=0; duiData->type; i++) // Cherche l'objet actif
+	{
+		if((x==duiData->x)&&(y==duiData->y)&&((type|DUI_ACTIVE)==duiData->type))
+		break;
+		duiData++;
+	}
+	if(!duiData->type) return; // Pas d'objet actif
+	
+	struct _duiDATA *duiFindData=duiData;
+	switch(action)
+	{
+		case DUI_PREV:
+			for(; --i>=0;) // Cherche l'objet précédent
+			{
+				duiFindData--;
+				if((x==duiFindData->x)&&(y==duiFindData->y)&&(type==duiFindData->type)&&duiFindData->text)
+				{
+					duiFindData->type|=DUI_ACTIVE;
+					duiData->type&=~DUI_ACTIVE;
+					return;
+				}
+			}
+			break;
+		case DUI_NEXT:
+		for(i++; (++duiFindData)->type; i++) // Cherche l'objet suivant
+		{
+			if((x==duiFindData->x)&&(y==duiFindData->y)&&(type==duiFindData->type)&&duiFindData->text)
+			{
+				duiFindData->type|=DUI_ACTIVE;
+				duiData->type&=~DUI_ACTIVE;
+				return;
+			}
+		}
+		break;
+	}
+}
+*/
+
+void Dui::edit(byte btn)
+{
+}
+
+byte Dui::exec(byte btn) // Traite l'appui sur un bouton
 // -> 2 = effacer et redessiner l'écran ; 1 = juste redessiner ; 0 = ne rien faire
 {
-  Serial.println("duiExec");
-  struct _duiDATA *duiData = duiCurrentPage;
-  byte i;
-  for(i=0; duiData->type; i++)
-  {
-    if((duiData->in&~DUI_ACTIVE)==btn)
-    {
-      if(!(duiData->in&DUI_ACTIVE)||(duiData->type&DUI_ACTIVE))
-      {
-        switch((int)duiData->out)
-        {
-          case 0 : break;
-          case DUI_PREV : duiChangeActive(DUI_PREV,i); return 1;
-          case DUI_NEXT : duiChangeActive(DUI_NEXT,i); return 1;
-          default : duiCurrentPage=duiData->out; return 2;
-        }
-      }
-    }
-    duiData++;
-  }
-  return 0;
+	Serial.println("exec");
+	struct _duiDATA *duiData = currentPage;
+	byte i;
+	// Recherche un champ éditable en cours d'édition
+	for(i=0; duiData->type; i++)
+	{
+		if(duiData->type==DUI_EDIT|DUI_ACTIVE)
+		{
+			edit(btn);
+			return 0;
+		}
+		duiData++;
+	}
+
+	// Recherche un bouton actif	
+	for(i=0; duiData->type; i++)
+	{
+		if((duiData->in&~DUI_ACTIVE)==btn)
+		{
+			if(!(duiData->in&DUI_ACTIVE)||(duiData->type&DUI_ACTIVE))
+			{
+				switch((int)duiData->out)
+				{
+					case 0 : break;
+					case DUI_PREV : changeActive(DUI_PREV,i); return 1;
+					case DUI_NEXT : changeActive(DUI_NEXT,i); return 1;
+					default : currentPage=duiData->out; return 2;
+				}
+			}
+		}
+		duiData++;
+	}
+	return 0;
 }
 
-void Dui::duiDisplay(byte clearDisplay) // Affiche une page de l'interface utilisateur
+void Dui::display(byte clearDisplay) // Affiche une page de l'interface utilisateur
 {
-  Serial.println("duiDisplay");
+  Serial.println("display");
   if(clearDisplay) lcd->clear();
-  struct _duiDATA *duiData = duiCurrentPage;
+  struct _duiDATA *duiData = currentPage;
   byte i,xx,yy,sel;
   xx=99;
   yy=99;
-//  duiActiveItem=99;
   for(i=0; duiData->type; i++) // Tant qu'il y a quelque chose à afficher
   {
-    if(duiData->text&&(duiData->type!=DUI_CHANGE))
+    if(duiData->text&&(duiData->type!=DUI_CHANGE)) // N'affiche pas les objetts CHANGE qui ne sont pas actifs
     {
       lcd->setCursor(duiData->x+(((duiData->type==DUI_SEL)||(duiData->type==DUI_LIST))?1:0), duiData->y); // Décalage pour le curseur
       lcd->print(duiData->text);
-//      if(((duiData->type==DUI_SEL)||(duiData->type==DUI_LIST))&&(duiActiveItem==99))
-//        duiActiveItem=i;
     }
     duiData++;
   }
-  duiSelectDisplay(true);
+  selectDisplay(true);
 }
 
 // --------------------------------------------------------
 // Les points d'entrée de DUI
 // --------------------------------------------------------
 
-Dui::Dui(duiDATA* currentPage) // Constructeur
-{
-	duiCurrentPage=currentPage;
-}
-
 void Dui::begin(LiquidCrystal* _lcd,byte width,byte height) // Initialiseur
 {
 	lcd=_lcd;
-  lcd->begin(width,height);
-  lcd->noCursor();
-  lcd->noBlink();
-  duiDisplay(true);
+	lcd->begin(width,height);
+	lcd->noCursor();
+	lcd->noBlink();
+	display(true);
 }
 
-void Dui::go() // Gestionnaire d'évènements
+void Dui::do() // Gestionnaire d'évènements
 {
-  byte button=readButtons();
-  if(button)
-  {
-    byte ret=duiExec(button);
-    while(readButtons());
-    if(ret) duiDisplay(ret==2);
-  }
+	byte button=readButtons();
+	if(button)
+	{
+		byte ret=exec(button);
+		while(readButtons());
+		if(ret) display(ret==2);
+	}
 }
--- Dui.h
+++ Dui.h
@@ -4,9 +4,10 @@
 #ifndef Dui_h
 #define Dui_h
 
+#define MAX_EDIT_SIZE 16 // Taille maximum d'un champ éditable
+
 #include "Arduino.h"
 #include <LiquidCrystal.h>
-
 
 #define DUI_KEY_NONE   0
 #define DUI_KEY_UP     1
@@ -17,7 +18,22 @@
 #define DUI_KEY_ESC    5 // Nouveau nom pour la touche SELECT
 #define DUI_KEY_RST    6
 #define DUI_KEY_OK     6 // Nouveau nom pour la touche RESET
-// !!! ATTENTION : pour les chiffres, ne pas utiliser 0  pour le '0' sans en tenir compte dans le code
+#define DUI_KEY_0 '0'
+#define DUI_KEY_1 '1'
+#define DUI_KEY_2 '2'
+#define DUI_KEY_3 '3'
+#define DUI_KEY_4 '4'
+#define DUI_KEY_5 '5'
+#define DUI_KEY_6 '6'
+#define DUI_KEY_7 '7'
+#define DUI_KEY_8 '8'
+#define DUI_KEY_9 '9'
+#define DUI_KEY_A 'A'
+#define DUI_KEY_B 'B'
+#define DUI_KEY_C 'C'
+#define DUI_KEY_D 'D'
+#define DUI_KEY_STAR '*'	// Ou définie comme ESC
+#define DUI_KEY_HASH '#'	// Ou définie comme OK
 
 // Objets
 #define DUI_TEXT 1    // Texte
@@ -26,6 +42,7 @@
 #define DUI_CHANGE 13 // Champ qui change cycliquement
 #define DUI_BTN 14      // Bouton sélectionnable
 
+#define DUI_EDIT 30     // Champ éditable
 #define DUI_NUM 31      // Valeur numérique
 #define DUI_ALPHA 32    // Valeur alphabétique
 #define DUI_ALPHANUM 33 // Valeur alphanumérique
@@ -41,32 +58,42 @@
 #define DUI_RIGHT 4
 #define DUI_INIT 5 // Appelé en entrant
 #define DUI_EXIT 6 // Appelé en sortant
+#define DUI_OK 7 // Appelé en cas de validation
 
+// Critères de recherche
+#define DUI_FIND_ACTIVE 0
+#define DUI_FIND_PREV 1
+#define DUI_FIND_NEXT 2
+#define DUI_FIND_FIRST 3
+#define DUI_FIND_LAST 4
 
 typedef struct _duiDATA
 {
-  byte x;
-  byte y;
-  byte type;
-  char *text;
-  byte in;
-  struct _duiDATA* out;
+	byte x;
+	byte y;
+	byte type;
+	char *text;
+	byte in;
+	struct _duiDATA* out;
 } duiDATA;
 
 class Dui
 {
 	public:
-		Dui(duiDATA* currentPage);
+		Dui(duiDATA* _currentPage) : currentPage(_currentPage){};
 		void begin(LiquidCrystal* _lcd,byte width,byte height);
-		void go();
+		void do();
 	private:
-		duiDATA* duiCurrentPage;
+		duiDATA* currentPage;
 		LiquidCrystal* lcd;
+		char editData[MAX_EDIT_SIZE+1]; // Buffer pour l'édition et l'affichage des champs éditables
 		byte readButtons();  // Lecture des boutons
-		void duiSelectDisplay(byte on); // Affiche ou efface le curseur de sélection
-		void duiChangeActive(byte action,byte index); // Change le texte actif d'un bouton
-		byte duiExec(byte btn); // Traite l'appui sur un bouton
-		void duiDisplay(byte clearDisplay); // Affiche une page de l'interface utilisateur
+		void selectDisplay(byte on); // Affiche ou efface le curseur de sélection
+		void changeActive(byte action,byte index); // Change le texte actif d'un bouton
+		byte exec(byte btn); // Traite l'appui sur un bouton
+		void display(byte clearDisplay); // Affiche une page de l'interface utilisateur
+		void edit(byte btn);
+		char findObject(byte type,byte index)
 };
 
 #endif
